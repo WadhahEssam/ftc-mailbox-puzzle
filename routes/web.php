86ef93e84616320bea5566c25d9cac9b9a4f5319
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-02-22 23:45:08 +0300

    creating the messages class and passing them to the mailbox page

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,33 @@
 
 use Illuminate\Http\Request;
 
-
 Route::get('/', function () {
-    return view('mailbox');
+    $inbox = new Inbox;
+    return view('mailbox', ['messages'=>$inbox->getAllMessages()]);
 }); 
 
 Route::get('/checkPassword', function (Request $request) {
     return view('mailbox', ['password'=>$request->password]);
 });
+
+Route::get('/test', function () {
+    $inbox = new Inbox;
+    return $inbox->getAllMessages();
+});
+
+
+class Inbox {
+    public $messages;
+    function __construct() {
+        $this->messages = [
+            ['title'=>'Message #1', 'content'=>'This is the content for first inbox message', 'date'=>'Sat 11:35'],
+            ['title'=>'Message #1', 'content'=>'This is the content for first inbox message', 'date'=>'Sat 11:35'],
+            ['title'=>'Message #1', 'content'=>'This is the content for first inbox message', 'date'=>'Sat 11:35'],
+            ['title'=>'Message #1', 'content'=>'This is the content for first inbox message', 'date'=>'Sat 11:35'],
+        ];
+    }
+
+    public function getAllMessages() {
+        return $this->messages;
+    }
+}
